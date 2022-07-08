303d1e6e83a0a44dde3d3186f6c06c619cc65bf7
José Moreno <josem3748@gmail.com>
2022-07-07 23:25:22 -0300

    Desafío: Catálogo con MAPS y Promises - Corregido

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,35 +8,37 @@ import "./styles/ItemList.css";
 const estrellas = (qestrellas) => {
   let stars = [];
   for (let i = 1; i <= qestrellas; i++) {
-    stars.push(<FontAwesomeIcon icon={faStar} color="green" />);
+    stars.push(<FontAwesomeIcon key={i} icon={faStar} color="green" />);
   }
   return stars;
 };
 
 function ItemList(props) {
   return (
-    <div className="card p-3">
-      <div className="d-flex flex-row mb-3">
-        <img className="mx-2" src={props.img} width="70" />
-        <div className="d-flex flex-column ml-2">
-          <span>{props.name}</span>
-          <span className="text-black-50">{props.category}</span>
-          <span className="ratings">{estrellas(props.stars)}</span>
+    <div className="col-md-4">
+      <div className="card p-3">
+        <div className="d-flex flex-row mb-3">
+          <img className="mx-2" src={props.img} alt={props.name} width="70" />
+          <div className="d-flex flex-column ml-2">
+            <span>{props.name}</span>
+            <span className="text-black-50">{props.category}</span>
+            <span className="ratings">{estrellas(props.stars)}</span>
+          </div>
         </div>
-      </div>
-      <h6>{props.description}</h6>
-      <div className="d-flex justify-content-between install mt-3">
-        <ItemCount initial={props.initial} stock={props.stock} />
-      </div>
-      <div>
-        <button type="button" className="btn btn-primary">
-          Add to cart
-        </button>
-      </div>
-      <div>
-        <button type="button" className="btn btn-secondary">
-          More info
-        </button>
+        <h6>{props.description}</h6>
+        <div className="d-flex justify-content-between install mt-3">
+          <ItemCount initial={props.initial} stock={props.stock} />
+        </div>
+        <div>
+          <button type="button" className="btn btn-primary">
+            Add to cart
+          </button>
+        </div>
+        <div>
+          <button type="button" className="btn btn-secondary">
+            More info
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,14 +1,17 @@
 import ItemList from "../components/ItemList";
 import "./styles/ItemListContainer.css";
 import Items from "../components/Item";
+import React, { useState, useEffect } from "react";
+import customFetch from "../components/customFetch";
 
 const ItemListContainer = (props) => {
-  return (
-    <div className="container my-5">
-      <div className="row">
-        <h1 className="text-center">{props.greeting}</h1>
-        {Items.map((item, index) => (
-          <div className="col-md-4">
+  let [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    customFetch(2000)
+      .then(() =>
+        setArr(
+          Items.map((item, index) => (
             <ItemList
               key={index}
               img={item.img}
@@ -19,8 +22,16 @@ const ItemListContainer = (props) => {
               initial={item.initial}
               stock={item.stock}
             />
-          </div>
-        ))}
+          ))
+        )
+      )
+      .catch((err) => alert(err));
+  }, []);
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <h1 className="text-center">{props.greeting}</h1>
+        {arr}
       </div>
     </div>
   );
